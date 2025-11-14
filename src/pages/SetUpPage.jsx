c3853fe64dc55b9fd7dcc5f2f6215736ca9b2536
yuraup <o721@naver.com>
2025-11-14 10:39:59 +0900

    feat: 셋업페이지 애니메이션 추가

--- src/pages/SetUpPage.jsx
+++ src/pages/SetUpPage.jsx
@@ -1,3 +1,4 @@
+import { motion as Motion } from 'framer-motion';
 import { useState } from 'react';
 import Button from '../components/common/Button';
 import { MIN_CAR, MAX_CAR, MIN_ROUND, MAX_ROUND, MAX_NAME_LENGTH } from '../constants/setUpNumbers';
@@ -34,68 +35,76 @@ export default function SetUpPage() {
   };
 
   return (
-    <div className="flex h-full w-full justify-center">
-      <form className="bg-ink flex h-full w-64 flex-col items-center gap-14 overflow-scroll py-12 text-center">
-        <img src="/assets/heart.png" alt="heart" className="h-9 w-10" />
-        <p className="font-basic text-4xl font-bold">게임 준비</p>
-        <div>
-          <p className="font-basic bg mb-6 text-xl font-medium">경쟁할 자동차 수</p>
-          <span className="font-basic flex items-center justify-between">
-            <button
-              type="button"
-              onClick={handleCarNumber}
-              className="text-ink h-8 w-8 rounded-md bg-pink-300 disabled:opacity-30"
-              disabled={carNumber === 1}
-            >
-              -
-            </button>
-            <p className="font-bold">{carNumber}</p>
-            <button
-              type="button"
-              onClick={handleCarNumber}
-              className="text-ink h-8 w-8 rounded-md bg-pink-300 disabled:opacity-30"
-              disabled={carNumber === 2}
-            >
-              +
-            </button>
-          </span>
-        </div>
-        <div>
-          <p className="font-basic mb-6 text-xl font-medium">진행할 라운드 수</p>
-          <span className="font-basic flex items-center justify-between">
-            <button
-              type="button"
-              onClick={handleRoundNumber}
-              className="text-ink h-8 w-8 rounded-md bg-pink-300 disabled:opacity-30"
-              disabled={roundNumber === 1}
-            >
-              -
-            </button>
-            <p className="font-bold">{roundNumber}</p>
-            <button
-              type="button"
-              onClick={handleRoundNumber}
-              className="text-ink h-8 w-8 rounded-md bg-pink-300 disabled:opacity-30"
-              disabled={roundNumber === 5}
-            >
-              +
-            </button>
-          </span>
-        </div>
-        <div>
-          <p className="font-basic mb-6 text-xl font-medium">내 자동차 이름</p>
-          <input
-            placeholder={`자동차 이름을 ${MAX_NAME_LENGTH} 자 이내로 입력해주세요`}
-            value={carName}
-            onChange={e => setCarName(e.target.value)}
-            maxLength={MAX_NAME_LENGTH}
-            className="font-basic h-12 w-60 rounded-md bg-white text-center text-xs font-medium text-pink-300"
-          />
-        </div>
-        <Button type="submit" disabled={carName.trim().length === 0} onClick={hanldeSubmit}>
-          시작하기
-        </Button>
-      </form>
-    </div>
+    <Motion.div
+      initial={{ opacity: 0, y: 0 }}
+      animate={{ opacity: 1, y: 0 }}
+      exit={{ opacity: 0, y: 0 }}
+      transition={{ duration: 0.4 }}
+      className="flex h-full w-full justify-center"
+    >
+      <div className="flex h-full w-full justify-center">
+        <form className="bg-ink flex h-full w-64 flex-col items-center gap-14 overflow-scroll py-12 text-center">
+          <img src="/assets/heart.png" alt="heart" className="h-9 w-10" />
+          <p className="font-basic text-4xl font-bold">게임 준비</p>
+          <div>
+            <p className="font-basic bg mb-6 text-xl font-medium">경쟁할 자동차 수</p>
+            <span className="font-basic flex items-center justify-between">
+              <button
+                type="button"
+                onClick={handleCarNumber}
+                className="text-ink h-8 w-8 rounded-md bg-pink-300 disabled:opacity-30"
+                disabled={carNumber === 1}
+              >
+                -
+              </button>
+              <p className="font-bold">{carNumber}</p>
+              <button
+                type="button"
+                onClick={handleCarNumber}
+                className="text-ink h-8 w-8 rounded-md bg-pink-300 disabled:opacity-30"
+                disabled={carNumber === 2}
+              >
+                +
+              </button>
+            </span>
+          </div>
+          <div>
+            <p className="font-basic mb-6 text-xl font-medium">진행할 라운드 수</p>
+            <span className="font-basic flex items-center justify-between">
+              <button
+                type="button"
+                onClick={handleRoundNumber}
+                className="text-ink h-8 w-8 rounded-md bg-pink-300 disabled:opacity-30"
+                disabled={roundNumber === 1}
+              >
+                -
+              </button>
+              <p className="font-bold">{roundNumber}</p>
+              <button
+                type="button"
+                onClick={handleRoundNumber}
+                className="text-ink h-8 w-8 rounded-md bg-pink-300 disabled:opacity-30"
+                disabled={roundNumber === 5}
+              >
+                +
+              </button>
+            </span>
+          </div>
+          <div>
+            <p className="font-basic mb-6 text-xl font-medium">내 자동차 이름</p>
+            <input
+              placeholder={`자동차 이름을 ${MAX_NAME_LENGTH} 자 이내로 입력해주세요`}
+              value={carName}
+              onChange={e => setCarName(e.target.value)}
+              maxLength={MAX_NAME_LENGTH}
+              className="font-basic h-12 w-60 rounded-md bg-white text-center text-xs font-medium text-pink-300"
+            />
+          </div>
+          <Button type="submit" disabled={carName.trim().length === 0} onClick={hanldeSubmit}>
+            시작하기
+          </Button>
+        </form>
+      </div>
+    </Motion.div>
   );
 }
