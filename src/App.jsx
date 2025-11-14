ed2c717c3c45f2f0e9d533413e6571fdb9c31157
yuraup <o721@naver.com>
2025-11-14 10:39:26 +0900

    chore: framer motion 애니메이션 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,22 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, useLocation } from 'react-router-dom';
 import { LandingPage } from './pages/LandingPage';
 import SetUpPage from './pages/SetUpPage';
 import GamePage from './pages/GamePage';
 import ResultPage from './pages/ResultPage';
+import { AnimatePresence } from 'framer-motion';
 
 export default function App() {
+  const location = useLocation();
   return (
     <div className="app-shell">
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/setup" element={<SetUpPage />} />
-        <Route path="/game" element={<GamePage />} />
-        <Route path="/result" element={<ResultPage />} />
-      </Routes>
+      <AnimatePresence mode="wait">
+        <Routes location={location} key={location.pathname}>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/setup" element={<SetUpPage />} />
+          <Route path="/game" element={<GamePage />} />
+          <Route path="/result" element={<ResultPage />} />
+        </Routes>
+      </AnimatePresence>
     </div>
   );
 }
