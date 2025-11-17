becf90c00d87c2c389c5e6f7ae6bcaee3f0d3c43
yuraup <o721@naver.com>
2025-11-17 14:40:54 +0900

    style: 모달 배경 중복 스타일 제거

--- src/components/modals/RandomModal.jsx
+++ src/components/modals/RandomModal.jsx
@@ -4,7 +4,7 @@ function RandomModal({ onNext }) {
   return (
     <Modal>
       <div
-        className="bg-light/90 relative z-10 flex h-72 w-72 flex-col items-center justify-center"
+        className="relative z-10 flex h-72 w-72 flex-col items-center justify-center"
         onClick={e => e.stopPropagation()}
       >
         <p className="font-basic text-ink font-bold">랜덤 숫자를 뽑습니다.</p>
